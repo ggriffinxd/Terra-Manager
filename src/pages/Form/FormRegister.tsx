21d0e25e10cc600477be4e81159b179b4ba6c2f4
i.gabreeu <aomedagabreeu601@gmail.com>
2024-07-19 19:48:11 -0300

    fix: formRegister to culture

--- src/pages/Form/FormRegister.tsx
+++ src/pages/Form/FormRegister.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import LabelInputRadioYesNo from "../../components/LabelInputRadioYesNo/LabelInputRadioYesNo";
 import * as S from "./styles";
 
 const FormRegister: React.FC = () => {
+  const [sharedState, setSharedState] = useState<boolean>(false);
   return (
     <S.FormContainer>
       <S.LabelInput>
-        <LabelInputRadioYesNo
-          title="Sua fazenda contém análise de solo?"
-          labelYes="Sim"
-          labelNot="Não"
-        />
+        {sharedState ? null : (
+          <LabelInputRadioYesNo
+            title="Sua fazenda contém análise de solo?"
+            labelYes="Sim"
+            labelNot="Não"
+            sharedState={sharedState}
+            setSharedState={setSharedState}
+          />
+        )}
       </S.LabelInput>
     </S.FormContainer>
   );
