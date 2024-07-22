601ad7a0fa69d083151ca6dd375490c173c78ac7
Sander-dev <felipe.sander350@gmail.com>
2024-07-22 16:07:22 -0300

    fix: remove insignificant lines at style of login

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -64,60 +63,5 @@ export const CheckboxContainer = styled.div`
   max-height: 30px;
   .p {
     font-style: normal;
-  }
-`;
-
-export const RememberMeLabel = styled.label`
-  display: inline-block;
-  padding-left: 8px;
-  cursor: pointer;
-`;
-
-export const ForgotPasswordLink = styled.a`
-  color: #007bff;
-  transition: color 0.15s ease-in-out;
-  &:hover,
-  &:focus {
-    color: #0056b3;
-  }
-`;
-
-export const SubmitButton = styled.button`
-  display: block;
-  width: 100%;
-  border-radius: 8px;
-  background-color: #007bff;
-  padding: 12px;
-  font-size: 12px;
-  font-weight: 500;
-  text-transform: uppercase;
-  color: white;
-  box-shadow: 0 4px 9px -4px rgba(59, 113, 202, 0.25);
-  transition: background-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-
-  &:hover,
-  &:focus {
-    background-color: #0056b3;
-    box-shadow: 0 8px 9px -4px rgba(59, 113, 202, 0.3),
-      0 4px 18px 0 rgba(59, 113, 202, 0.2);
-  }
-
-  &:active {
-    background-color: #004494;
-    box-shadow: 0 8px 9px -4px rgba(59, 113, 202, 0.3),
-      0 4px 18px 0 rgba(59, 113, 202, 0.2);
-  }
-`;
-
-export const RegisterLink = styled.a`
-  display: block;
-  margin-top: 24px;
-  text-align: center;
-  color: #007bff;
-  transition: color 0.15s ease-in-out;
-
-  &:hover,
-  &:focus {
-    color: #0056b3;
   }
 `;
