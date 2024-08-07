d8b0e017892383c48dca6a10ff83a524173ac3e8
Sander-dev <felipe.sander350@gmail.com>
2024-08-07 19:44:49 -0300

    feat: inital page from home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      babamozovo <br />
+      <Link to={"/"}>Para login</Link>
+    </div>
+  );
+}
+
+export default Home;
